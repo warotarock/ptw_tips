ba947a1e5049b73ce63b3333e80309d79c9049f3
user <user@mail.com>
2017-10-25 16:18:02 +0900

    ・グローバルな関数のみで処理していたものを、Mainクラスで処理を行うように修正

--- tips/basic_model_converting/main.ts
+++ tips/basic_model_converting/main.ts
@@ -14,124 +14,135 @@ namespace BasicModelConverting {
         indices: List<int>;
     }
 
-    // Main process
+    // Main
 
-    window.onload = () => {
+    class Main {
 
-        var fileName = 'sample_basic_model.dae';
-        var outFileName = getExtensionChangedFileName('../temp/' + fileName, 'json');
+        execute() {
 
-        document.getElementById('content').innerHTML = 'Out put will be located ' + outFileName;
+            var fileName = 'sample_basic_model.dae';
+            var outFileName = this.getExtensionChangedFileName('../temp/' + fileName, 'json');
 
-        var collada_loader = new THREE.ColladaLoader();
+            document.getElementById('content').innerHTML = 'Out put will be located ' + outFileName;
 
-        // Parsing by collada loader
-        collada_loader.load(
-            fileName,
-            function (threeJSCollada) {
-                var helper = new Converters.ThreeJSColladaConverterHelper();
-                let sceneData = helper.parse(threeJSCollada);
+            var collada_loader = new THREE.ColladaLoader();
 
-                // Converting
-                var convetedModels = convert(sceneData.staticMeshes);
+            // Parsing by collada loader
+            collada_loader.load(
+                fileName,
+                (threeJSCollada) => {
 
-                // Output
-                output(convetedModels, outFileName);
+                    var parser = new Converters.ThreeJSColladaParser();
+                    let sceneData = parser.parse(threeJSCollada);
 
-                document.getElementById('content').innerHTML = 'Out put done ' + outFileName;
-            }
-        );
-    };
+                    // Converting
+                    var convetedModels = this.convert(sceneData.staticMeshes);
 
-    function convert(staticMeshes: List<Converters.Mesh>): List<ConvertedModel> {
+                    // Output
+                    this.output(convetedModels, outFileName);
 
-        var convetedModels = new List<ConvertedModel>();
-        for (var meshIndex = 0; meshIndex < staticMeshes.length; meshIndex++) {
-            var mesh = staticMeshes[meshIndex];
-
-            var vertices = [];
-            for (var i = 0; i < mesh.vertices.length; i++) {
-                var modelVertex = mesh.vertices[i];
-
-                vertices.push(modelVertex.position[0]);
-                vertices.push(modelVertex.position[1]);
-                vertices.push(modelVertex.position[2]);
-
-                vertices.push(modelVertex.normal[0]);
-                vertices.push(modelVertex.normal[1]);
-                vertices.push(modelVertex.normal[2]);
-
-                for (var k = 0; k < modelVertex.texcoords.length; k++) {
-                    vertices.push(modelVertex.texcoords[k][0]);
-                    vertices.push(modelVertex.texcoords[k][1]);
+                    document.getElementById('content').innerHTML = 'Out put done ' + outFileName;
                 }
-            }
+            );
 
-            var indices = [];
-            for (var i = 0; i < mesh.faces.length; i++) {
-                var modelFace = mesh.faces[i];
+        }
 
-                for (var k = 0; k < modelFace.vertexIndeces.length; k++) {
-                    indices.push(modelFace.vertexIndeces[k]);
+        convert(staticMeshes: List<Converters.Mesh>): List<ConvertedModel> {
+
+            var convetedModels = new List<ConvertedModel>();
+            for (var meshIndex = 0; meshIndex < staticMeshes.length; meshIndex++) {
+                var mesh = staticMeshes[meshIndex];
+
+                var vertices = [];
+                for (var i = 0; i < mesh.vertices.length; i++) {
+                    var modelVertex = mesh.vertices[i];
+
+                    vertices.push(modelVertex.position[0]);
+                    vertices.push(modelVertex.position[1]);
+                    vertices.push(modelVertex.position[2]);
+
+                    vertices.push(modelVertex.normal[0]);
+                    vertices.push(modelVertex.normal[1]);
+                    vertices.push(modelVertex.normal[2]);
+
+                    for (var k = 0; k < modelVertex.texcoords.length; k++) {
+                        vertices.push(modelVertex.texcoords[k][0]);
+                        vertices.push(modelVertex.texcoords[k][1]);
+                    }
                 }
+
+                var indices = [];
+                for (var i = 0; i < mesh.faces.length; i++) {
+                    var modelFace = mesh.faces[i];
+
+                    for (var k = 0; k < modelFace.vertexIndeces.length; k++) {
+                        indices.push(modelFace.vertexIndeces[k]);
+                    }
+                }
+
+                convetedModels.push({
+                    name: mesh.name,
+                    vertexStride: (3 + 3) + (2 * mesh.vertices[0].texcoords.length),
+                    vertices: vertices,
+                    indices: indices
+                });
             }
 
-            convetedModels.push({
-                name: mesh.name,
-                vertexStride: (3 + 3) + (2 * mesh.vertices[0].texcoords.length),
-                vertices: vertices,
-                indices: indices
+            return convetedModels;
+        }
+
+        output(convetedMeshes: List<ConvertedModel>, outFileName: string) {
+
+            var tab1 = '  ';
+            var tab2 = '    ';
+            var tab3 = '      ';
+
+            var out = [];
+
+            out.push('{')
+
+            out.push(tab1 + '\"models\": {')
+
+            for (var i = 0; i < convetedMeshes.length; i++) {
+                var convetedMesh = convetedMeshes[i];
+
+                out.push(tab2 + '\"' + convetedMesh.name + '\": {');
+                out.push(tab3 + '\"vertexStride\": ' + convetedMesh.vertexStride);
+                out.push(tab3 + ', \"vertex\": ' + JSON.stringify(convetedMesh.vertices, this.jsonStringifyReplacer));
+                out.push(tab3 + ', \"index\": ' + JSON.stringify(convetedMesh.indices));
+                out.push(tab2 + '}' + (i < convetedMeshes.length - 1 ? ',' : ''));
+            }
+
+            out.push(tab1 + '}')
+
+            out.push('}')
+
+            fs.writeFile(outFileName, out.join('\r\n'), function (error) {
+                if (error != null) {
+                    alert('error : ' + error);
+                }
             });
         }
 
-        return convetedModels;
-    }
+        getExtensionChangedFileName(fileName: string, newExtension) {
 
-    function output(convetedMeshes: List<ConvertedModel>, outFileName: string) {
-
-        var tab1 = '  ';
-        var tab2 = '    ';
-        var tab3 = '      ';
-
-        var out = [];
-
-        out.push('{')
-
-        out.push(tab1 + '\"models\": {')
-
-        for (var i = 0; i < convetedMeshes.length; i++) {
-            var convetedMesh = convetedMeshes[i];
-
-            out.push(tab2 + '\"' + convetedMesh.name + '\": {');
-            out.push(tab3 + '\"vertexStride\": ' + convetedMesh.vertexStride);
-            out.push(tab3 + ', \"vertex\": ' + JSON.stringify(convetedMesh.vertices, jsonStringifyReplacer));
-            out.push(tab3 + ', \"index\": ' + JSON.stringify(convetedMesh.indices));
-            out.push(tab2 + '}' + (i < convetedMeshes.length - 1 ? ',' : ''));
+            return (fileName.match(/(.*)(?:\.([^.]+$))/))[1] + '.' + newExtension;
         }
 
-        out.push(tab1 + '}')
+        jsonStringifyReplacer(key: string, value: any): any {
 
-        out.push('}')
-
-        fs.writeFile(outFileName, out.join('\r\n'), function (error) {
-            if (error != null) {
-                alert('error : ' + error);
+            if (typeof value === 'number') {
+                return Number(value.toFixed(4));
             }
-        });
-    }
-
-    function getExtensionChangedFileName(fileName: string, newExtension) {
-
-        return (fileName.match(/(.*)(?:\.([^.]+$))/))[1] + '.' + newExtension;
-    }
-
-    function jsonStringifyReplacer(key: string, value: any): any {
-
-        if (typeof value === 'number') {
-            return Number(value.toFixed(4));
-        }
-        else {
-            return value;
+            else {
+                return value;
+            }
         }
     }
+
+    window.onload = () => {
+
+        let main = new Main();
+        main.execute();
+    };
 }
--- tips/object_animation_converting/main.ts
+++ tips/object_animation_converting/main.ts
@@ -3,260 +3,275 @@ var fs = require('fs');
 
 namespace ObjectAnimationConverter {
 
-    window.onload = () => {
+    class Main {
 
-        var fileName = 'sample_obj_animation.blend';
-        var outFileName = getExtensionChangedFileName('../temp/' + fileName, 'json');
+        execute() {
 
-        var request = new XMLHttpRequest();
-        request.open('GET', fileName, true);
-        request.responseType = 'arraybuffer';
-        request.addEventListener('load',
-            (e: Event) => {
-                // read a blend file
-                var blendFile = BlendFileReader.readBlendFile(request.response);
+            var fileName = 'sample_obj_animation.blend';
+            var outFileName = this.getExtensionChangedFileName('../temp/' + fileName, 'json');
 
-                // execute converting
-                var convetedData = convert(blendFile);
-                output(convetedData, outFileName);
+            document.getElementById('content').innerHTML = 'Out put will be located ' + outFileName;
+
+            var request = new XMLHttpRequest();
+            request.open('GET', fileName, true);
+            request.responseType = 'arraybuffer';
+            request.addEventListener('load',
+                (e: Event) => {
+                    // read a blend file
+                    var blendFile = BlendFileReader.readBlendFile(request.response);
+
+                    // execute converting
+                    var convetedData = this.convert(blendFile);
+                    this.output(convetedData, outFileName);
+
+                    document.getElementById('content').innerHTML = 'Out put done ' + outFileName;
+                }
+            );
+            request.send();
+        }
+
+        getExtensionChangedFileName(fileName: string, newExtension) {
+
+            return (fileName.match(/(.*)(?:\.([^.]+$))/))[1] + '.' + newExtension;
+        }
+
+        jsonStringifyReplacer(key: string, value: any): any {
+
+            if (typeof value === 'number') {
+                return Number(value.toFixed(4));
             }
-        );
-        request.send();
-    };
-
-    function getExtensionChangedFileName(fileName: string, newExtension) {
-
-        return (fileName.match(/(.*)(?:\.([^.]+$))/))[1] + '.' + newExtension;
-    }
-
-    function jsonStringifyReplacer(key: string, value: any): any {
-
-        if (typeof value === 'number') {
-            return Number(value.toFixed(4));
-        }
-        else {
-            return value;
-        }
-    }
-
-    function floatArrayToArray(array: Float32Array) {
-        var result = [];
-        for (var i = 0; i < array.length; i++) {
-            result.push(array[i]);
-        }
-        return result;
-    }
-
-    function convert(blendFile: BlendFileReader.ReadBlendFileResult): Dictionary<any> {
-
-        var bheadDictionary = new Dictionary<BlendFileReader.BHead>();
-        Enumerable.From(blendFile.bheadList)
-            .ForEach(bhead => bheadDictionary[bhead.old] = bhead);
-
-        var bAction_TypeInfo = blendFile.dna.getStructureTypeInfo('bAction');
-        var bAction_BHeads = Enumerable.From(blendFile.bheadList)
-            .Where(bh => bh.SDNAnr == bAction_TypeInfo.sdnaIndex)
-            .ToArray();
-
-        var result = new List<any>();
-
-        // for each bAction
-        for (var i = 0; i < bAction_BHeads.length; i++) {
-            var bAction_BHead = bAction_BHeads[i];
-            var bAction_DataSet = blendFile.dna.createDataSetFromBHead(bAction_BHead);
-
-            var animation = {
-                name: bAction_DataSet.id.name.substr(2),
-                curves: []
-            };
-
-            var lastGroupName = null;
-            var channelIndex = 0;
-
-            // for each fCurve in bAction
-            var fCurve_Address = bAction_DataSet.curves.first;
-            while (true) {
-                var fCurve_BHead = bheadDictionary[fCurve_Address];
-                var fCurve_DataSet = blendFile.dna.createDataSetFromBHead(fCurve_BHead);
-
-                var bActionGroup_BHead = bheadDictionary[fCurve_DataSet.grp];
-                var bActionGroup_DataSet = blendFile.dna.createDataSetFromBHead(bActionGroup_BHead);
-
-                var bezTriple_Bhead = bheadDictionary[fCurve_DataSet.bezt];
-                var bezTriple_DataSet = blendFile.dna.createDataSetFromBHead(bezTriple_Bhead);
-
-                var points = [];
-                for (var k = 0; k < bezTriple_DataSet.elementCount; k++) {
-                    var bezt = bezTriple_DataSet[k];
-                    points.push(
-                        [
-                            [bezt.vec[0], bezt.vec[1], bezt.vec[2]],
-                            [bezt.vec[3], bezt.vec[4], bezt.vec[5]],
-                            [bezt.vec[6], bezt.vec[7], bezt.vec[8]]
-                        ]
-                    );
-                }
-
-                var isBoneAction = StringIsNullOrEmpty(getCurveName(bActionGroup_DataSet.name, fCurve_DataSet.array_index));
-
-                var groupName: string;
-                var channelName: string;
-                if (isBoneAction) {
-                    groupName = bActionGroup_DataSet.name
-                    if (lastGroupName != groupName) {
-                        lastGroupName = groupName;
-                        channelIndex = 0;
-                    }
-                    channelName = getBoneCurveName(channelIndex);
-                    channelIndex++;
-                }
-                else {
-                    groupName = "Object";
-                    channelName = getCurveName(bActionGroup_DataSet.name, fCurve_DataSet.array_index);
-                }
-
-                var curve = {
-                    group: groupName.replace(/_/g, '.'),
-                    channel: channelName,
-                    array_index: fCurve_DataSet.array_index,
-                    points: points
-                    //selected: (fCurve_DataSet.flag & 0x02) != 0,
-                    //active: (fCurve_DataSet.flag & 0x04) != 0,
-                    //locked: (fCurve_DataSet.flag & 0x08) != 0,
-                    //mute: (fCurve_DataSet.flag & 0x10) != 0,
-                    //modifire: (fCurve_DataSet.flag & 0x100) != 0
-                };
-                animation.curves.push(curve);
-
-                if (fCurve_Address == bAction_DataSet.curves.last) {
-                    break;
-                }
-                else {
-                    fCurve_Address = fCurve_DataSet.next;
-                }
+            else {
+                return value;
             }
-
-            result.push(animation);
         }
 
-        return result;
-    }
+        floatArrayToArray(array: Float32Array) {
+            var result = [];
+            for (var i = 0; i < array.length; i++) {
+                result.push(array[i]);
+            }
+            return result;
+        }
 
-    function output(convetedData: List<any>, outFileName: string) {
+        convert(blendFile: BlendFileReader.ReadBlendFileResult): Dictionary<any> {
 
-        var out = [];
+            var bheadDictionary = new Dictionary<BlendFileReader.BHead>();
+            Enumerable.From(blendFile.bheadList)
+                .ForEach(bhead => bheadDictionary[bhead.old] = bhead);
 
-        out.push("{");
-
-        for (var i = 0; i < convetedData.length; i++) {
-            var animation = convetedData[i];
-
-            out.push("  \"" + animation.name + "\": {");
-
-            var channelGroup = Enumerable.From(<List<any>>animation.curves)
-                .GroupBy(curve => curve.group)
-                .Select(group => ({
-                    name: group.Key(),
-                    curves: group.source
-                }))
-                .OrderBy(group => group.name)
+            var bAction_TypeInfo = blendFile.dna.getStructureTypeInfo('bAction');
+            var bAction_BHeads = Enumerable.From(blendFile.bheadList)
+                .Where(bh => bh.SDNAnr == bAction_TypeInfo.sdnaIndex)
                 .ToArray();
 
-            for (var groupIndex = 0; groupIndex < channelGroup.length; groupIndex++) {
-                var group = channelGroup[groupIndex];
+            var result = new List<any>();
 
-                out.push("    \"" + group.name + "\": {");
+            // for each bAction
+            for (var i = 0; i < bAction_BHeads.length; i++) {
+                var bAction_BHead = bAction_BHeads[i];
+                var bAction_DataSet = blendFile.dna.createDataSetFromBHead(bAction_BHead);
 
-                for (var k = 0; k < group.curves.length; k++) {
-                    var curve = group.curves[k];
+                var animation = {
+                    name: bAction_DataSet.id.name.substr(2),
+                    curves: []
+                };
 
-                    var output_carve = {
-                        ipoType: 2,
-                        lastTime: 0.0,
-                        lastIndex: 0,
-                        curve: curve.points
+                var lastGroupName = null;
+                var channelIndex = 0;
+
+                // for each fCurve in bAction
+                var fCurve_Address = bAction_DataSet.curves.first;
+                while (true) {
+                    var fCurve_BHead = bheadDictionary[fCurve_Address];
+                    var fCurve_DataSet = blendFile.dna.createDataSetFromBHead(fCurve_BHead);
+
+                    var bActionGroup_BHead = bheadDictionary[fCurve_DataSet.grp];
+                    var bActionGroup_DataSet = blendFile.dna.createDataSetFromBHead(bActionGroup_BHead);
+
+                    var bezTriple_Bhead = bheadDictionary[fCurve_DataSet.bezt];
+                    var bezTriple_DataSet = blendFile.dna.createDataSetFromBHead(bezTriple_Bhead);
+
+                    var points = [];
+                    for (var k = 0; k < bezTriple_DataSet.elementCount; k++) {
+                        var bezt = bezTriple_DataSet[k];
+                        points.push(
+                            [
+                                [bezt.vec[0], bezt.vec[1], bezt.vec[2]],
+                                [bezt.vec[3], bezt.vec[4], bezt.vec[5]],
+                                [bezt.vec[6], bezt.vec[7], bezt.vec[8]]
+                            ]
+                        );
+                    }
+
+                    var isBoneAction = StringIsNullOrEmpty(this.getCurveName(bActionGroup_DataSet.name, fCurve_DataSet.array_index));
+
+                    var groupName: string;
+                    var channelName: string;
+                    if (isBoneAction) {
+                        groupName = bActionGroup_DataSet.name
+                        if (lastGroupName != groupName) {
+                            lastGroupName = groupName;
+                            channelIndex = 0;
+                        }
+                        channelName = this.getBoneCurveName(channelIndex);
+                        channelIndex++;
+                    }
+                    else {
+                        groupName = "Object";
+                        channelName = this.getCurveName(bActionGroup_DataSet.name, fCurve_DataSet.array_index);
+                    }
+
+                    var curve = {
+                        group: groupName.replace(/_/g, '.'),
+                        channel: channelName,
+                        array_index: fCurve_DataSet.array_index,
+                        points: points
+                        //selected: (fCurve_DataSet.flag & 0x02) != 0,
+                        //active: (fCurve_DataSet.flag & 0x04) != 0,
+                        //locked: (fCurve_DataSet.flag & 0x08) != 0,
+                        //mute: (fCurve_DataSet.flag & 0x10) != 0,
+                        //modifire: (fCurve_DataSet.flag & 0x100) != 0
                     };
+                    animation.curves.push(curve);
 
-                    out.push("      \"" + curve.channel + "\": "
-                        + JSON.stringify(output_carve, jsonStringifyReplacer)
-                        + (k < group.curves.length - 1 ? ',' : '')
-                    );
+                    if (fCurve_Address == bAction_DataSet.curves.last) {
+                        break;
+                    }
+                    else {
+                        fCurve_Address = fCurve_DataSet.next;
+                    }
                 }
 
-                out.push("    }" + (groupIndex < channelGroup.length - 1 ? ',' : ''));
+                result.push(animation);
             }
 
-            out.push("  }" + (i < convetedData.length - 1 ? ',' : ''));
+            return result;
         }
 
-        out.push("}");
+        output(convetedData: List<any>, outFileName: string) {
 
-        fs.writeFile(outFileName, out.join('\r\n'), function (error) {
-            if (error != null) {
-                alert('error : ' + error);
+            var out = [];
+
+            out.push("{");
+
+            for (var i = 0; i < convetedData.length; i++) {
+                var animation = convetedData[i];
+
+                out.push("  \"" + animation.name + "\": {");
+
+                var channelGroup = Enumerable.From(<List<any>>animation.curves)
+                    .GroupBy(curve => curve.group)
+                    .Select(group => ({
+                        name: group.Key(),
+                        curves: group.source
+                    }))
+                    .OrderBy(group => group.name)
+                    .ToArray();
+
+                for (var groupIndex = 0; groupIndex < channelGroup.length; groupIndex++) {
+                    var group = channelGroup[groupIndex];
+
+                    out.push("    \"" + group.name + "\": {");
+
+                    for (var k = 0; k < group.curves.length; k++) {
+                        var curve = group.curves[k];
+
+                        var output_carve = {
+                            ipoType: 2,
+                            lastTime: 0.0,
+                            lastIndex: 0,
+                            curve: curve.points
+                        };
+
+                        out.push("      \"" + curve.channel + "\": "
+                            + JSON.stringify(output_carve, this.jsonStringifyReplacer)
+                            + (k < group.curves.length - 1 ? ',' : '')
+                        );
+                    }
+
+                    out.push("    }" + (groupIndex < channelGroup.length - 1 ? ',' : ''));
+                }
+
+                out.push("  }" + (i < convetedData.length - 1 ? ',' : ''));
             }
-        });
-    }
 
-    function getCurveName(actionGroupName: string, array_index: int) {
-        if (actionGroupName == "Location") {
+            out.push("}");
+
+            fs.writeFile(outFileName, out.join('\r\n'), function (error) {
+                if (error != null) {
+                    alert('error : ' + error);
+                }
+            });
+        }
+
+        getCurveName(actionGroupName: string, array_index: int) {
+
+            if (actionGroupName == "Location") {
+                if (array_index == 0) {
+                    return "locationX";
+                }
+                else if (array_index == 1) {
+                    return "locationY";
+                }
+                else if (array_index == 2) {
+                    return "locationZ";
+                }
+            }
+            else if (actionGroupName == "Rotation") {
+                if (array_index == 0) {
+                    return "rotationX";
+                }
+                else if (array_index == 1) {
+                    return "rotationY";
+                }
+                else if (array_index == 2) {
+                    return "rotationZ";
+                }
+            }
+            else if (actionGroupName == "Scaling") {
+                if (array_index == 0) {
+                    return "scalingX";
+                }
+                else if (array_index == 1) {
+                    return "scalingY";
+                }
+                else if (array_index == 2) {
+                    return "scalingZ";
+                }
+            }
+
+            return null;
+        }
+
+        getBoneCurveName(array_index: int) {
+
             if (array_index == 0) {
-                return "locationX";
+                return "quatW";
             }
             else if (array_index == 1) {
-                return "locationY";
+                return "quatX";
             }
             else if (array_index == 2) {
-                return "locationZ";
+                return "quatY";
+            }
+            else if (array_index == 3) {
+                return "quatZ";
+            }
+            else if (array_index == 4) {
+                return "locX";
+            }
+            else if (array_index == 5) {
+                return "locY";
+            }
+            else if (array_index == 6) {
+                return "locZ";
             }
         }
-        else if (actionGroupName == "Rotation") {
-            if (array_index == 0) {
-                return "rotationX";
-            }
-            else if (array_index == 1) {
-                return "rotationY";
-            }
-            else if (array_index == 2) {
-                return "rotationZ";
-            }
-        }
-        else if (actionGroupName == "Scaling") {
-            if (array_index == 0) {
-                return "scalingX";
-            }
-            else if (array_index == 1) {
-                return "scalingY";
-            }
-            else if (array_index == 2) {
-                return "scalingZ";
-            }
-        }
-
-        return null;
     }
 
-    function getBoneCurveName(array_index: int) {
-        if (array_index == 0) {
-            return "quatW";
-        }
-        else if (array_index == 1) {
-            return "quatX";
-        }
-        else if (array_index == 2) {
-            return "quatY";
-        }
-        else if (array_index == 3) {
-            return "quatZ";
-        }
-        else if (array_index == 4) {
-            return "locX";
-        }
-        else if (array_index == 5) {
-            return "locY";
-        }
-        else if (array_index == 6) {
-            return "locZ";
-        }
-    }
+    window.onload = () => {
+
+        let main = new Main();
+        main.execute();
+    };
 }
